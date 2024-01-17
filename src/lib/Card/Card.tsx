70218f8674bf0cf9f997ec5a8d51b82fa923d2a8
jeffcaf1 <jeff@doablehq.com>
2024-01-17 15:31:05 -0800

    Remove default title, descr, img from Card

--- src/lib/Card/Card.tsx
+++ src/lib/Card/Card.tsx
@@ -5,9 +5,9 @@ import { CardProps } from "../ArticleLayouts/Template-0/types";
 const Card = ({
   variant = "small",
   link = "/",
-  image = "https://images.unsplash.com/photo-1698728877137-963bca5b5310?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  title = "Porsche is adding Google to its cars as VW’s software problems worsen",
-  description = "The sports car maker said it would use native Google apps in its future cars, starting in just two years. Meanwhile, VW is laying off thousands of people from its in-house software division",
+  image = "",
+  title = "",
+  description = "",
 }: CardProps) => {
   return (
     <a href={link} className={`card card-${variant}`}>
